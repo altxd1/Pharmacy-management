c0416ec2ebfea9f48a19ba9c89b58c1a6e5505ee
altxd1 <aaouadouabdelmonaim@gmail.com>
2024-05-28 20:41:32 +0100

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,40 @@
+<?php
+  require "php/db_connection.php";
+
+  if($con) {
+    $query = "UPDATE admin_credentials SET IS_LOGGED_IN = 'false'";
+    $result = mysqli_query($con, $query);
+  }
+  
+session_start();
+
+$_SESSION = array();
+
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// Détruire la session
+session_destroy();
+
+// Rediriger vers la page de connexion
+header("Location: login.php");
+exit;
+?>
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Logout</title>
+    <script src="js/restrict.js"></script>
+  </head>
+  <body>
+
+  </body>
+</html>
